9d2694e8d4a48ca8c9b77865f2713cb51bc08540
Andreas Linnert <andreas@linnertmedia.de>
2024-12-12 00:12:09 +0100

    Revert adding GitHup Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kali/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
