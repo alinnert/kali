04eaa643c12ff5806c893f664e5ccc60247572f4
Andreas Linnert <andreas@linnertmedia.de>
2024-12-11 01:50:29 +0100

    Populate home page and hide litter cal for now

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import AppNavItem from '@/components/app-nav/AppNavItem.vue'
-import { CalendarRange, House, Smile, Trash2 } from 'lucide-vue-next'
+import { CalendarRange, House, Smile } from 'lucide-vue-next'
 </script>
 
 <template>
   <div class="grid grid-cols-[80px,340px,1fr] gap-x-2 fixed inset-0">
     <div class="border-r dark:border-r-gray-600 print:hidden bg-white dark:bg-gray-800">
-      <div class="text-center py-3 font-bold italic uppercase select-none dark:text-white">Kali</div>
+      <div class="text-center py-3 font-bold italic uppercase select-none dark:text-white">
+        Kali
+      </div>
       <nav class="flex flex-col gap-y-1.5 p-1.5">
         <AppNavItem to="/" exact-match label="Startseite" :icon="House" />
         <AppNavItem to="/feiertage" label="Feiertage" :icon="Smile" />
         <AppNavItem to="/kalender/schicht" label="Schicht- kalender" :icon="CalendarRange" />
-        <AppNavItem to="/kalender/müllabfuhr" label="Abfuhr- kalender" :icon="Trash2" />
+        <!-- <AppNavItem to="/kalender/müllabfuhr" label="Abfuhr- kalender" :icon="Trash2" /> -->
       </nav>
     </div>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,38 @@ import AppHeadline from '@/components/basic/AppHeadline.vue'
 <template>
   <AppHeadline>Kali</AppHeadline>
 
-  <p>
-    Kali ist ein Werkzeug, um verschiedene Arten von druckbaren Kalendern zu erzeugen. Wähle links
-    den gewünschten Kalender aus, konfiguriere ihn nach deinen Bedürfnissen und drucke ihn aus.
-  </p>
+  <p>Kali ist ein Werkzeug, um verschiedene Arten von druckbaren Kalendern zu erzeugen.</p>
+
+  <AppHeadline :level="2">Anleitung</AppHeadline>
+
+  <ol class="list-decimal my-2 ml-6 [&>*]:pl-1">
+    <li>
+      Konfiguriere bei Bedarf deine
+      <RouterLink to="/feiertage" class="text-sky-700 dark:text-sky-400 underline"
+        >Feiertage</RouterLink
+      >. Wähle dazu erst das <strong>Bundesland</strong> aus und dann ggf. welche speziellen
+      Feiertage angezeigt werden sollen oder nicht.
+    </li>
+    <li>
+      Wähle dann in der Navigation links den gewünschten Kalender aus und konfiguriere ihn nach
+      deinen Bedürfnissen.
+    </li>
+    <li>
+      Anschließend kann er über den Button "<em>Drucken</em>" (oben links) ausgedruckt werden. Es
+      wird empfohlen im Drucken-Dialog <strong>a)</strong> die Einstellung "<em
+        >Hintergrundgrafiken drucken</em
+      >" zu <strong>aktivieren</strong> sowie <strong>b)</strong> die Einstellung "<em
+        >Kopf- und Fußzeilen drucken</em
+      >" zu <strong>deaktivieren</strong>.
+    </li>
+  </ol>
+
+  <AppHeadline :level="2">Über Kali</AppHeadline>
+
+  <ul class="[&_a]:text-sky-700 dark:[&_a]:text-sky-400 [&_a]:underline">
+    <li><a href="https://github.com/alinnert/kali">Quellcode</a></li>
+    <li>
+      <a href="https://github.com/alinnert/kali/issues">Fehler melden und andere Anregungen</a>
+    </li>
+  </ul>
 </template>
